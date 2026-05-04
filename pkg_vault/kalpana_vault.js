7d495c66da5cb60cd6024f11f5194f605ac92937
Madusha Perera <madusha@perfectbss.com>
2026-05-04 15:58:26 +0530

    Fix: Finalizing Persistent WASM migration and purging legacy LogicShield references

--- pkg_vault/kalpana_vault.js
+++ pkg_vault/kalpana_vault.js
@@ -24,6 +24,36 @@ async function instantiate(module, imports = {}) {
   const { exports } = await WebAssembly.instantiate(module, adaptedImports);
   const memory = exports.memory || imports.env.memory;
   const adaptedExports = Object.setPrototypeOf({
+    setState(re, im, o3, p4) {
+      // assembly/index/setState(~lib/typedarray/Float32Array, ~lib/typedarray/Float32Array, ~lib/typedarray/Float32Array, ~lib/typedarray/Float32Array) => void
+      re = __retain(__lowerTypedArray(Float32Array, 5, 2, re) || __notnull());
+      im = __retain(__lowerTypedArray(Float32Array, 5, 2, im) || __notnull());
+      o3 = __retain(__lowerTypedArray(Float32Array, 5, 2, o3) || __notnull());
+      p4 = __lowerTypedArray(Float32Array, 5, 2, p4) || __notnull();
+      try {
+        exports.setState(re, im, o3, p4);
+      } finally {
+        __release(re);
+        __release(im);
+        __release(o3);
+      }
+    },
+    getStateRe() {
+      // assembly/index/getStateRe() => ~lib/typedarray/Float32Array
+      return __liftTypedArray(Float32Array, exports.getStateRe() >>> 0);
+    },
+    getStateIm() {
+      // assembly/index/getStateIm() => ~lib/typedarray/Float32Array
+      return __liftTypedArray(Float32Array, exports.getStateIm() >>> 0);
+    },
+    getStateO3() {
+      // assembly/index/getStateO3() => ~lib/typedarray/Float32Array
+      return __liftTypedArray(Float32Array, exports.getStateO3() >>> 0);
+    },
+    getStateP4() {
+      // assembly/index/getStateP4() => ~lib/typedarray/Float32Array
+      return __liftTypedArray(Float32Array, exports.getStateP4() >>> 0);
+    },
     writeRIF(t, emb) {
       // assembly/index/writeRIF(f32, ~lib/typedarray/Float32Array) => void
       emb = __lowerTypedArray(Float32Array, 5, 2, emb) || __notnull();
@@ -50,6 +80,14 @@ async function instantiate(module, imports = {}) {
     while (end - start > 1024) string += String.fromCharCode(...memoryU16.subarray(start, start += 1024));
     return string + String.fromCharCode(...memoryU16.subarray(start, end));
   }
+  function __liftTypedArray(constructor, pointer) {
+    if (!pointer) return null;
+    return new constructor(
+      memory.buffer,
+      __getU32(pointer + 4),
+      __dataview.getUint32(pointer + 8, true) / constructor.BYTES_PER_ELEMENT
+    ).slice();
+  }
   function __lowerTypedArray(constructor, id, align, values) {
     if (values == null) return 0;
     const
@@ -63,6 +101,23 @@ async function instantiate(module, imports = {}) {
     exports.__unpin(buffer);
     return header;
   }
+  const refcounts = new Map();
+  function __retain(pointer) {
+    if (pointer) {
+      const refcount = refcounts.get(pointer);
+      if (refcount) refcounts.set(pointer, refcount + 1);
+      else refcounts.set(exports.__pin(pointer), 1);
+    }
+    return pointer;
+  }
+  function __release(pointer) {
+    if (pointer) {
+      const refcount = refcounts.get(pointer);
+      if (refcount === 1) exports.__unpin(pointer), refcounts.delete(pointer);
+      else if (refcount) refcounts.set(pointer, refcount - 1);
+      else throw Error(`invalid refcount '${refcount}' for reference '${pointer}'`);
+    }
+  }
   function __notnull() {
     throw TypeError("value must not be null");
   }
@@ -75,11 +130,24 @@ async function instantiate(module, imports = {}) {
       __dataview.setUint32(pointer, value, true);
     }
   }
+  function __getU32(pointer) {
+    try {
+      return __dataview.getUint32(pointer, true);
+    } catch {
+      __dataview = new DataView(memory.buffer);
+      return __dataview.getUint32(pointer, true);
+    }
+  }
   return adaptedExports;
 }
 export const {
   memory,
   initEngine,
+  setState,
+  getStateRe,
+  getStateIm,
+  getStateO3,
+  getStateP4,
   writeRIF,
   readRIF,
   getVersion,
