213f451e56dd58a66e9c2b8a097f467bb8ef9e1f
Madusha Perera <madusha@perfectbss.com>
2026-05-04 16:16:23 +0530

    v3.0.2: Final Path Fixes for GitHub Pages Compatibility

--- pkg_vault/kalpana_vault.js
+++ pkg_vault/kalpana_vault.js
@@ -158,4 +158,4 @@ export const {
     else { return await globalThis.WebAssembly.compileStreaming(globalThis.fetch(url)); }
   })(), {
   }
-))(new URL("release.wasm", import.meta.url));
+))(new URL("kalpana_vault.wasm", import.meta.url));
